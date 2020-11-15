0b8680f8c82b1c5678ae70e2a110c49b741d1bef
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-11-15 14:05:43 +0100

    Integration test for basic usage

---
+++ tests/src/test/java/it/mulders/puml/tests/BasicUsageIT.java
@@ -0,0 +1,31 @@
+package it.mulders.puml.tests;
+
+/*
+ * Copyright 2020 Maarten Mulders
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
+import com.soebes.itf.jupiter.extension.MavenTest;
+import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+
+import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
+
+@MavenJupiterExtension
+public class BasicUsageIT {
+    @MavenTest
+    void should_generate_diagram(final MavenExecutionResult result) {
+        assertThat(result).isSuccessful();
+    }
+}

4b01d7def7d830a72cbf22128878d9e32cf0d930
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-05-17 09:06:20 +0200

    Don't use deprecated method getBaseDir anymore

--- tests/src/test/java/it/mulders/puml/tests/Issue23IT.java
+++ tests/src/test/java/it/mulders/puml/tests/Issue23IT.java
@@ -36,7 +36,7 @@ public class Issue23IT {
     void should_generate_png_file(final MavenExecutionResult result) throws IOException {
         assertThat(result).isSuccessful();
 
-        final Path baseDir = Paths.get(result.getMavenProjectResult().getBaseDir().toURI());
+        final Path baseDir = Paths.get(result.getMavenProjectResult().getTargetProjectDirectory().toURI());
         final Path outputDirectory = baseDir.resolve(Paths.get("target", "site"));
         Assertions.assertThat(outputDirectory)
                 .exists()

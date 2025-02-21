a328fa07eed32d5d4c35a6d0f1f358f3e51920b7
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-02-21 13:53:09 +0100

    Integration test for failing on invalid input

---
+++ tests/src/test/java/it/mulders/puml/tests/FailOnInvalidInputIT.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 Maarten Mulders
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.mulders.puml.tests;
+
+import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
+
+import com.soebes.itf.jupiter.extension.MavenGoal;
+import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
+import com.soebes.itf.jupiter.extension.MavenTest;
+import com.soebes.itf.jupiter.maven.MavenExecutionResult;
+
+@MavenJupiterExtension
+public class FailOnInvalidInputIT {
+    @MavenTest
+    @MavenGoal("process-resources")
+    void should_fail_on_invalid_input(final MavenExecutionResult result) {
+        assertThat(result).isFailure();
+    }
+}
